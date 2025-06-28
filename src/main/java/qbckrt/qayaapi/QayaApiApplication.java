8f133896e8609ace4fa37820a69e61aabace23e1
q-bckrt <pom.schc@egmail.com>
2025-06-28 21:35:40 +0200

    initialized project

---
+++ src/main/java/qbckrt/qayaapi/QayaApiApplication.java
@@ -0,0 +1,24 @@
+package qbckrt.qayaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
+
+@SpringBootApplication
+public class QayaApiApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .directory("src/main/resources")
+                .load();
+
+        // Set environment variables from .env file
+        System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
+        SpringApplication.run(QayaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/qbckrt/qayaapi/QayaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package qbckrt.qayaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QayaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
