ca1aa3e18d3b0a7cc15f2ab38f8279f0598624a2
q-bckrt <pom.schc@egmail.com>
2025-06-29 18:18:41 +0200

    fixed wrong DDL migration

--- src/main/resources/db/migration/V5__MODIFY_CURRENCY_PK_AND_ADD_TO_USER.sql
+++ src/main/resources/db/migration/V5__MODIFY_CURRENCY_PK_AND_ADD_TO_USER.sql
@@ -3,14 +3,16 @@ SET search_path TO public;
 --
 ALTER TABLE accounts DROP CONSTRAINT IF EXISTS fk_accounts_currency;
 ALTER TABLE accounts DROP COLUMN IF EXISTS currency_id;
+--
+ALTER TABLE currencies DROP CONSTRAINT IF EXISTS pk_currencies;
+ALTER TABLE currencies DROP COLUMN if EXISTS id;
+--
+ALTER TABLE currencies ADD CONSTRAINT pk_currencies PRIMARY KEY (code);
+--
 ALTER TABLE accounts ADD COLUMN currency VARCHAR(16);
 ALTER TABLE accounts ALTER COLUMN currency SET NOT NULL;
 ALTER TABLE accounts
     ADD CONSTRAINT fk_accounts_currency FOREIGN KEY (currency) REFERENCES currencies(code);
---
-ALTER TABLE currencies DROP CONSTRAINT IF EXISTS pk_currencies;
-ALTER TABLE currencies DROP COLUMN if EXISTS id;
-ALTER TABLE currencies ADD CONSTRAINT pk_currencies PRIMARY KEY (code);
 --
 ALTER TABLE users ADD COLUMN currency VARCHAR(16);
 ALTER TABLE users ALTER COLUMN currency SET NOT NULL;
